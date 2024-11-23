791f172e4a2e1ff7744e86cc0151a399d005560c
Amin <104219888+Amin-Gz@users.noreply.github.com>
2024-11-23 21:42:18 +0330

    removing older codes

--- src/assets/old site/views/layouts/script/date_picker.blade.php
+++
@@ -1,3 +0,0 @@
-<!-- INTERNAL data picker -->
-<script src="{{URL::asset('assets/plugins/date-picker/persian-date.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/date-picker/persian-datepicker.js')}}"></script>
