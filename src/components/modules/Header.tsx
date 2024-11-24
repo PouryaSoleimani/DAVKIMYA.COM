3f23835953ac4a95665ec9c2152055decb96d98c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-24 15:29:22 +0330

    HEADER

--- src/components/modules/Header.tsx
+++ src/components/modules/Header.tsx
@@ -30,11 +30,11 @@ const Header = () => {
     useEffect(() => {
         if (windowWidth > 1000) { setshowMenu(true) } else { setshowMenu(false) }
     }, [windowWidth]);
-
+ 
 
     return (
         <header>
-            {/*  contact  */}
+            {/*  CONTACT INFO  */}
             <div className="border-bottom">
                 <div
                     className="d-flex flex-wrap gap-4 align-items-center justify-content-md-between justify-content-center container py-3">
@@ -83,59 +83,54 @@ const Header = () => {
                     </div>
                 </div>
             </div>
-
-
-            {/* <!-- menu && logo --> */}
+            {/* <!-- MENU AND LOGO --> */}
             <div className='bg-[#474841]'>
-                {/* <!-- menu --> */}
+                {/* <!-- MENU --> */}
                 <nav className="navbar navbar-expand-lg bg-body-tertiary container p-0 bg-[#474841]">
                     <div className="container-fluid bg-[#474841] ">
-                        {/* <!-- logo --> */}
+                        {/* <!-- LOGO --> */}
                         <Link className="navbar-brand" href="/">
                             <Image src={HeaderLogo} alt="daav" className="img-fluid py-3" width={90} height={90} />
                         </Link>
-
-                        {/* //* NAVBAR */}
+                        {/* SEARCH MODAL AND HAMBURGER MENU TOGGLER */}
                         <div className="d-flex align-items-center gap-3">
-
                             <button onClick={showMenuHandler} className="navbar-toggler primary-border" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-list primary-color" viewBox="0 0 16 16">
                                     <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"></path>
                                 </svg>
                             </button>
 
-                            <button className="d-flex d-lg-none ms-4 cursor" role="search" data-bs-toggle="modal" data-bs-target="#exampleModal" >
+                            <button data-bs-toggle="modal" data-bs-target="#exampleModal" className="d-flex d-lg-none ms-4 cursor" role="search" >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-search text-white" viewBox="0 0 16 16">
                                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
                                 </svg>
                             </button>
-
                         </div>
-
+                        {/* MENU */}
                         <div className={`${showMenu === false ? "collapse" : ""} navbar-collapse translate-y-2 lg:translate-y-3`}>
                             <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-4">
                                 <li className="nav-item">
-                                    <a className="nav-link primary-color active" aria-current="page" href="https://davkimya.com/en">Home</a>
+                                    <Link className="nav-link primary-color active" aria-current="page" href="/">Home</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link primary-color" href="https://davkimya.com/en/about-us">About us</a>
+                                    <Link className="nav-link primary-color" href="https://davkimya.com/en/about-us">About us</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link primary-color" href="https://davkimya.com/en/product">Product</a>
-                                </li>
-
-                                <li className="nav-item">
-                                    <a className="nav-link primary-color" href="https://davkimya.com/en/service">R&amp;D Services</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link primary-color" href="https://davkimya.com/en/faqs">Faqs</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link primary-color" href="https://davkimya.com/en/blogs">Event &amp; News</a>
+                                    <Link className="nav-link primary-color" href="https://davkimya.com/en/product">Product</Link>
                                 </li>
 
                                 <li className="nav-item">
-                                    <a className="nav-link primary-color" href="https://davkimya.com/en/contact-us">Contact us</a>
+                                    <Link className="nav-link primary-color" href="https://davkimya.com/en/service">R&amp;D Services</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link primary-color" href="https://davkimya.com/en/faqs">Faqs</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link primary-color" href="https://davkimya.com/en/blogs">Event &amp; News</Link>
+                                </li>
+
+                                <li className="nav-item">
+                                    <Link className="nav-link primary-color" href="https://davkimya.com/en/contact-us">Contact us</Link>
                                 </li>
                             </ul>
                             <form
@@ -155,14 +150,26 @@ const Header = () => {
                                 </svg>
                             </form>
                         </div>
-
-
-
-
-
                     </div>
                 </nav>
-                {/* <!-- </nav> --> */}
+            </div>
+            {/* <!-- SEARCH MODAL --> */}
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered ">
+                    <div className="modal-content border border-lime-400">
+                        <div className="modal-header ">
+                            <h1 className="modal-title fs-5 w-full text-center" id="exampleModalLabel">Search</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <input type="search" name="modalSearchInput" id="modalSearchInput" className='border-2 border-zinc-800 rounded-3xl w-full p-2 focus:outline-lime-500' />
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Search</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </header >
     )
