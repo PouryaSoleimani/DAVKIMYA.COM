147f7e9f8169ef2b9977627503e8d1015178fdaa
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-27 22:25:55 +0330

    ABOUT-US PAGE

--- src/components/pages/aboutUsPage/index.tsx
+++ src/components/pages/aboutUsPage/index.tsx
@@ -7,9 +7,10 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import Image from "next/image";
 import TOPIMAGE from "@/../public/images/about-us/TOP.jpg";
-import BOTTOMLOGO from"@/../public/images/about-us/BOTTOM_LOGO.jpg"
+import BOTTOMLOGO from "@/../public/images/about-us/BOTTOM__LOGO.webp"
 import Link from "next/link";
 import { useEffect } from "react";
+import { ImEarth } from "react-icons/im";
 // ^ COMPONENT
 const AboutUsPage = () => {
   console.log(i18next.language);
@@ -114,7 +115,7 @@ const AboutUsPage = () => {
           </div>
           <div className="col-lg-6 px-0">
             <div className="bg-black2 p-5 text-white">
-              <Image alt="desc" src={BOTTOMLOGO} width={800} height={600} />
+              <Image alt="desc" src={BOTTOMLOGO} width={90} height={600} />
               <div className="text-justify">
                 <p className="mx-0">
                   <strong>
@@ -161,10 +162,8 @@ const AboutUsPage = () => {
                 </p>
               </div>
             </div>
-            <div className="primary-bg p-5 text-dark">
-              <svg xmlns="http://www.w3.org/2000/svg" width="47" height="47" fill="currentColor" className="bi bi-globe-americas" viewBox="0 0 16 16">
-                <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484-.08.08-.162.158-.242.234-.416.396-.787.749-.         758 1.266.035.634.618.824 1.214 1.017.577.188 1. 168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z" />
-              </svg>
+            <div className="primary-bg px-5 pt-4 pb-5 text-dark">
+              <ImEarth className="w-16 h-16 my-4" />
               <div>
                 <p className="mx-0 text-justify">
                   <strong>
