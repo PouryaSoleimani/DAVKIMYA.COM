5f33abf0bb2cbb8a687b7a91f9925e576a0b418a
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-28 16:15:33 +0330

    OUR PRODUCTS - CARDS

--- src/components/pages/productPage/index.tsx
+++ src/components/pages/productPage/index.tsx
@@ -1,22 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
+// ^ OUR PRODUCTS PAGE =========================================================================================================================
 import Link from "next/link";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
-
+import { useLang } from "@/core/providers/langProvider";
+import { useTranslation } from "@/core/i18n/client";
+import Image from "next/image";
+import SAMPLEIMAGE from '@/../public/images/boxes/BOX1.jpg'
+// COMPONENT =================================================================================================================================
 const ProductPage = () => {
+  const { t } = useTranslation();
+  const { lng } = useLang();
   useEffect(() => { AOS.init(); AOS.refresh() }, []);
+
+  // RETURN =================================================================================================================================
   return (
     <>
       {/* TITLE */}
       <section data-aos="fade-down" data-aos-duration="2500" id="article-title" className="d-flex top align-items-center justify-content-center py-5 px-2 position-relative" data-sr-id="0" >
         <div className="container text-white fw-bold text-center my-5 py-5 position-relative">
-          <h1 className="display-1">Our Product</h1>
+          <h1 className="display-1">{t("ourProductsTitle")}</h1>
           <div className="d-flex align-items-center justify-content-center gap-1">
-            <Link href="/en/" className="d-block primary-color fw-bold text-decoration-none">Home</Link>
+            <Link href="/en/" className="d-block primary-color fw-bold text-decoration-none">{t("home")}</Link>
             <span>/</span>
             <Link href="/en/product" className="text-white-50">
-              Product
+              {t("product")}
             </Link>
           </div>
         </div>
@@ -27,9 +37,10 @@ const ProductPage = () => {
         <div className="container">
           <div className="text-center">
             <span className="h3">
-              Our Product
+              {t("ourProductsTitle")}
             </span>
           </div>
+
           <div className="row g-4 mt-5">
             <div className="col-xxl-3 col-md-4 col-sm-6">
               <div className="card_category">
@@ -38,18 +49,7 @@ const ProductPage = () => {
                     Polymeric Emulsion System
                   </Link>
                   <hr />
-                  <Link href="https://davkimya.com/en/product/36" className="title_h5">
-                    Adhesives and sealants
-                  </Link>
-                  <Link href="https://davkimya.com/en/product/37" className="title_h5">
-                    Paint and coating application
-                  </Link>
-                  <Link href="https://davkimya.com/en/product/38" className="title_h5">
-                    Tape and label
-                  </Link>
-                  <Link href="https://davkimya.com/en/product/39" className="title_h5">
-                    Textile application
-                  </Link>
+                  <Image src={SAMPLEIMAGE} width={600} height={600} alt="image" className="mt-3" />
                 </div>
               </div>
             </div>
@@ -60,24 +60,7 @@ const ProductPage = () => {
                     Polyurethanes
                   </Link>
                   <hr />
-                  <Link href="https://davkimya.com/en/product/41" className="title_h5">
-                    Polyester polyols
-                  </Link>
-                  <Link href="https://davkimya.com/en/product/42" className="title_h5">
-                    Spray polyurethane system
-                  </Link>
-                  <Link href="https://davkimya.com/en/product/43" className="title_h5">
-                    High-Density PU Foams
-                  </Link>
-                  <Link href="https://davkimya.com/en/product/44" className="title_h5">
-                    Rigid PU and PIR for Composite Panel Insulation
-                  </Link>
-                  <Link href="https://davkimya.com/en/product/45" className="title_h5">
-                    Industrial Insulation Foams
-                  </Link>
-                  <Link href="https://davkimya.com/en/product/46" className="title_h5">
-                    Bio-based Polyurethanes
-                  </Link>
+                  <Image src={SAMPLEIMAGE} width={600} height={600} alt="image" className="mt-3" />
                 </div>
               </div>
             </div>
@@ -88,15 +71,7 @@ const ProductPage = () => {
                     Engineered Polymeric Compounds
                   </Link>
                   <hr />
-                  <Link href="https://davkimya.com/en/product/54" className="title_h5">
-                    Plastics for the Lighting Industry
-                  </Link>
-                  <Link href="https://davkimya.com/en/product/59" className="title_h5">
-                    Electronic and Electrical Industry
-                  </Link>
-                  <Link href="https://davkimya.com/en/product/62" className="title_h5">
-                    Household Appliances and Automobile Industry
-                  </Link>
+                  <Image src={SAMPLEIMAGE} width={600} height={600} alt="image" className="mt-3" />
                 </div>
               </div>
             </div>
@@ -107,15 +82,7 @@ const ProductPage = () => {
                     Mastic and sealants
                   </Link>
                   <hr />
-                  <Link href="https://davkimya.com/en/product/67" className="title_h5">
-                    Acrylic copolymer sealants
-                  </Link>
-                  <Link href="https://davkimya.com/en/product/68" className="title_h5">
-                    Similar silicon sealants
-                  </Link>
-                  <Link href="https://davkimya.com/en/product/69" className="title_h5">
-                    MS polymer-based sealants
-                  </Link>
+                  <Image src={SAMPLEIMAGE} width={600} height={600} alt="image" className="mt-3" />
                 </div>
               </div>
             </div>
