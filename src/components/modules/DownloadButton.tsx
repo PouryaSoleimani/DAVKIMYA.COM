7be1b802013d50a5710742fd79e0644dad624604
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-29 23:33:29 +0330

    DOWNLOAD CATALOGUE

--- src/components/modules/DownloadButton.tsx
+++ src/components/modules/DownloadButton.tsx
@@ -1,11 +1,14 @@
 // ^ DONWLOAD BUTTON COMPONENT ===================================================================================================================
 import React from 'react'
 import { FaDownload } from "react-icons/fa6";
+// import TESTPDF from "https://davkimya.com/assets/uploads/setting/Slider/1402-05-07/photos/photo-17ee7b11900de071a6ca81f4ead407ae.jpg"
+import Link from 'next/link';
+
 const DownloadButton = () => {
    return (
-      <button className="group relative inline-flex items-center justify-center w-[50px] h-[50px] bg-gradient-to-r from-blue-500 to-green-500 text-white rounded-full shadow-lg transform scale-100 transition-transform duration-300 focus:outline-none focus:ring-4 focus:ring-blue-300">
-         <FaDownload className='w-10 h-10' />
-      </button>
+      <Link download={true} href="https://pouryasoleimani-adddesign.storage.c2.liara.space/DAV___KIMYA/TEST.pdf" className="group relative inline-flex items-center justify-center w-[50px] h-[50px] bg-gradient-to-r from-blue-500 to-green-500 text-white rounded-full shadow-lg transform scale-100 transition-transform focus:outline-none focus:ring-4 focus:ring-blue-300 hover:scale-105 duration-500" target="_blank" rel="noopener noreferrer">
+         <FaDownload className='w-6 h-6' />
+      </Link>
 
    )
 }
