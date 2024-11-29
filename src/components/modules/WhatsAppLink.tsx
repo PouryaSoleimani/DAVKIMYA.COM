aaef08972e146498cf25a2295ef751f71b7f6b5b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-11-29 23:33:48 +0330

    DOWNLOAD CATALOGUE

--- src/components/modules/WhatsAppLink.tsx
+++ src/components/modules/WhatsAppLink.tsx
@@ -8,7 +8,6 @@ const WhatsAppLink = () => {
       <section className="flex justify-center items-center  fixed right-12 bottom-16 z-[999] overflow-visible">
          <Link href="https://api.whatsapp.com/send?phone=+900000000000" className="group flex flex-col items-center justify-center p-2 rounded-md drop-shadow-xl bg-[#1ED760] from-gray-800 to-black text-white font-semibold hover:translate-y-3 hover:rounded-[50%]  transition-all duration-500 hover:from-[#331029] hover:to-[#310413]" >
             <FaWhatsapp className="w-10 h-10" />
-            {/* <h2>WhatsApp</h2> */}
             <span className="z-50 absolute -bottom-6 font-semibold  tracking-wide  text-transparent group-hover:text-black duration-500">
                WhatsApp
             </span>
