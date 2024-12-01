a4b54bb1f6b5f8df5ee9b9a76a9b6ba79c0957fd
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-12-01 21:19:33 +0330

    SEARCH MODAL

--- src/components/modules/Header.tsx
+++ src/components/modules/Header.tsx
@@ -104,7 +104,6 @@ const Header = () => {
                                     <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"></path>
                                 </svg>
                             </button>
-
                             <Button variant="ghost" onClick={() => setModalShow(true)} className="flex min-[1000px]:hidden items-center justify-center">
                                 <FaSearch className="text-white w-10 h-10 cursor-pointer translate-y-0" />
                             </Button>
