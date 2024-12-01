3c31cafed403b8178bb9b88025fded23c719631e
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-12-01 22:16:14 +0330

    MAP DEBUGGING

--- src/components/modules/Footer.tsx
+++ src/components/modules/Footer.tsx
@@ -33,7 +33,7 @@ const Footer = () => {
           {/* MAP */}
           <div className="col-lg-4">
             <div style={{ width: "100%" }}>
-                <Link href="https://www.gps.ie/">gps systems</Link>
+              {/* <Link href="https://www.gps.ie/">gps systems</Link> */}
               <iframe width="100%" height="300" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=41%C2%B016'04.7%22N%2027%C2%B056'26.5%22E+(Dav%20Kimya)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
               </iframe>
             </div>
