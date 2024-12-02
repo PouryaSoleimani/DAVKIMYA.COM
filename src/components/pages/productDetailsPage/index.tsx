ed1d951fcdbcf3a92bc758c8d4a35aa92c72673f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-12-02 12:27:03 +0330

    PRODUCT PDF PAGE

--- src/components/pages/productDetailsPage/index.tsx
+++ src/components/pages/productDetailsPage/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
+// ^ PRODUCT PDF PAGE COMPONENT ==================================================================================================================================
 import poructsDetails from "@/core/constants/useProductsDetails.json";
 import { useLang } from "@/core/providers/langProvider";
 import Link from "next/link";
@@ -10,12 +11,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useTranslation } from "@/core/i18n/client";
 
-type selectedTitle = {
-  title: string;
-  src: string;
-  description: string[];
-  products: string[];
-};
+type selectedTitle = { title: string; src: string; description: string[]; products: string[]; };
 
 const ProductDetailsPage = () => {
   const { t } = useTranslation();
@@ -32,52 +28,31 @@ const ProductDetailsPage = () => {
       ? title.split("%20").join(" ").toString()
       : title[0].split("%20").join(" ").toString()
     : "";
+
   const [selectedTitle, setSelectedTitle] = useState<selectedTitle>();
 
   useEffect(() => {
-    AOS.init();
-    AOS.refresh();
-    poructsDetails.map((item: selectedTitle) => {
-      t(item.title) === productTitle && setSelectedTitle(item);
-    });
+    AOS.init(); AOS.refresh();
+    poructsDetails.map((item: selectedTitle) => { t(item.title) === productTitle && setSelectedTitle(item); });
   }, []);
   return (
     <>
       {/* <!-- article-title --> */}
 
-      <section
-        id="article-title"
-        data-aos="fade-down"
-        data-aos-duration="2500"
-        data-sr-id="0"
-        className="d-flex top align-items-center justify-content-center py-5 px-2 position-relative"
-      >
+      <section id="article-title" data-aos="fade-down" data-aos-duration="2500" data-sr-id="0" className="d-flex top align-items-center justify-content-center py-5 px-2 position-relative" >
         <div className="container text-white fw-bold text-center my-5 py-5 position-relative">
           <h1 className="display-1">Our Product</h1>
           <div className="d-flex align-items-center justify-content-center gap-1">
-            <Link
-              href={`/${lng}/product`}
-              className="d-block primary-color fw-bold text-decoration-none"
-            >
-              Home
-            </Link>
+            <Link href={`/${lng}/product`} className="d-block primary-color fw-bold text-decoration-none"   > {t("home")} </Link>
             <span>/</span>
-            <Link href={`/`} className="text-white-50">
-              Product
-            </Link>
+            <Link href={`/`} className="text-white-50"> {t("product")}</Link>
           </div>
         </div>
       </section>
 
       {/* <!-- articles --> */}
 
-      <section
-        id="articles"
-        className="mt-5 py-5 left"
-        data-aos="fade-right"
-        data-aos-duration="2500"
-        data-sr-id="0"
-      >
+      <section id="articles" className="mt-5 py-5 left" data-aos="fade-right" data-aos-duration="2500" data-sr-id="0"  >
         <div className="container">
           <div className="text-center">
             <span className="h3">{productTitle}</span>
@@ -85,9 +60,7 @@ const ProductDetailsPage = () => {
           <div className="row g-4 mt-5">
             <div className="col-md-12">
               <div className="mb-3">
-                {selectedTitle?.description.map((item) => (
-                  <p key={item}>{t(item)}</p>
-                ))}
+                {selectedTitle?.description.map((item) => (<p key={item}>{t(item)}</p>))}
               </div>
               <hr />
               <h5 className="product_name_h5 text-center">
--- src/components/pages/productPdfPage/index.tsx
+++ src/components/pages/productPdfPage/index.tsx
@@ -47,64 +47,25 @@ const ProductPdfPage = () => {
                 </Link>
               </div>
               <div>
-                <Link
-                  href={`/en/product/DAVINYl-T5002/show/37`}
-                  className="d-flex align-items-center gap-1 cursor primary-color text-decoration-none fw-bold h5"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="32"
-                    height="32"
-                    fill="currentColor"
-                    className="flex-shrink-0 bi bi-arrow-right-short text-dark mt-1"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
-                    />
+                <Link href={`/en/product/DAVINYl-T5002/show/37`} className="d-flex align-items-center gap-1 cursor primary-color text-decoration-none fw-bold h5"     >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="flex-shrink-0 bi bi-arrow-right-short text-dark mt-1" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
                   </svg>
                   DAVINYl-T5002
                 </Link>
               </div>
               <div>
-                <Link
-                  href={`/en/product/DAVINYL-H5018/show/36`}
-                  className="d-flex align-items-center gap-1 cursor primary-color text-decoration-none fw-bold h5"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="32"
-                    height="32"
-                    fill="currentColor"
-                    className="flex-shrink-0 bi bi-arrow-right-short text-dark mt-1"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
-                    />
+                <Link href={`/en/product/DAVINYL-H5018/show/36`} className="d-flex align-items-center gap-1 cursor primary-color text-decoration-none fw-bold h5"  >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="flex-shrink-0 bi bi-arrow-right-short text-dark mt-1" viewBox="0 0 16 16"  >
+                    <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
                   </svg>
                   DAVINYL-H5018
                 </Link>
               </div>
               <div>
-                <Link
-                  href={`/en/product/DAVINYl-C5300/show/35`}
-                  className="d-flex align-items-center gap-1 cursor primary-color text-decoration-none fw-bold h5"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="32"
-                    height="32"
-                    fill="currentColor"
-                    className="flex-shrink-0 bi bi-arrow-right-short text-dark mt-1"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
-                    />
+                <Link href={`/en/product/DAVINYl-C5300/show/35`} className="d-flex align-items-center gap-1 cursor primary-color text-decoration-none fw-bold h5"                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="flex-shrink-0 bi bi-arrow-right-short text-dark mt-1" viewBox="0 0 16 16"   >
+                    <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
                   </svg>
                   DAVINYl-C5300
                 </Link>
