01e6bff9f7b507eeff9f4b5a7874af7642f85f3b
Amin-Gz <amingoudarzy@yahoo.com>
2024-12-06 22:13:23 +0000

    adding pm2 and http request packages to connect domain to the app

---
+++ proxy.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer({});
+const server = http.createServer((req, res) => {
+  proxy.web(req, res, { target: 'http://localhost:3000' });
+});
+
+server.listen(80, () => {
+  console.log('Proxy server listening on port 80');
+});
