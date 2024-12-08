2eb7f3c79f2b798aeaafc3608e6258270fcb94c4
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-12-08 22:28:53 +0330

    Footer Phone Number

--- src/components/modules/Footer.tsx
+++ src/components/modules/Footer.tsx
@@ -90,24 +90,6 @@ const Footer = () => {
                   <li className="mb-2">
                     <Link className="d-flex align-items-center gap-2 text-decoration-none text-white fw-bold" href={`/${lng}/contact-us`}  >
                       <FaArrowRight className="pl-1 text-lime-400 my-1.5" />
-                      <span>+90 534 043 36 46</span>
-                    </Link>
-                  </li>
-                  <li className="mb-2">
-                    <Link className="d-flex align-items-center gap-2 text-decoration-none text-white fw-bold" href={`/${lng}/contact-us`}  >
-                      <FaArrowRight className="pl-1 text-lime-400 my-1.5" />
-                      <span>+90 534 687 90 96</span>
-                    </Link>
-                  </li>
-                  <li className="mb-2">
-                    <Link className="d-flex align-items-center gap-2 text-decoration-none text-white fw-bold" href={`/${lng}/contact-us`}  >
-                      <FaArrowRight className="pl-1 text-lime-400 my-1.5" />
-                      <span>+90 506 503 43 38</span>
-                    </Link>
-                  </li>
-                  <li className="mb-2">
-                    <Link className="d-flex align-items-center gap-2 text-decoration-none text-white fw-bold" href={`/${lng}/contact-us`}    >
-                      <FaArrowRight className="pl-1 text-lime-400 my-1.5" />
                       <span>+90 506 503 43 38</span>
                     </Link>
                   </li>
