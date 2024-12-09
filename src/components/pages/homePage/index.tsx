8db0702bcfffc5a2113d08546609c9995aab8e16
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-12-09 13:50:47 +0330

    Why Choose Us

--- src/components/pages/homePage/index.tsx
+++ src/components/pages/homePage/index.tsx
@@ -155,13 +155,17 @@ const HomePage = () => {
         </div>
       </section>
 
-      <h2 className="text-center font-extrabold text-4xl bg-zinc-300 w-fit mx-auto py-4 px-16 rounded-xl border-2 border-lime-400"> Why Choose Us ?</h2>
+      <h2 className="text-center font-extrabold text-4xl  underline decoration-4 decoration-lime-400"> Why Choose Us ?</h2>
+      <span className="tracking-tighter text-[17px] font-sans text-center w-full block font-semibold text-zinc-600">Welcome to DavKimya- where innovation meets sustainability</span>
       <section className="wrapper my-20">
         <div className="scene">
           <div className="carousel keen-slider" ref={sliderRef}>
             <div className="carousel__cell number-slide1 flex flex-col items-start justify-start">
               <p className="text-start ">1</p>
               <h3>High-Quality, Consistent Products</h3>
+              <p className="text-muted text-lg">
+                We pride ourselves on producing top-grade emulsion resins that consistently meet the stringent quality standards of our industry. Our commitment ensures that your formulations are always reliable and high-performing.
+              </p>
             </div>
             <div className="carousel__cell number-slide2">2</div>
             <div className="carousel__cell number-slide3">3</div>
