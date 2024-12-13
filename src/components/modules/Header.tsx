73fd65ecec79713e5f37c3fc07b7eece17472ba6
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-12-13 20:49:27 +0330

    DROP DOWN HEADER

--- src/components/modules/Header.tsx
+++ src/components/modules/Header.tsx
@@ -117,26 +117,17 @@ const Header = () => {
                                 </li>
 
                                 {/* NEW DROPDOWN */}
-                                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                                    <Navbar.Collapse id="responsive-navbar-nav">
-                                        <Nav className="mr-auto">
-                                            <Nav.Link href="#features">Features</Nav.Link>
-                                            <NavDropdownMenu title="Dropdown 1" id="collasible-nav-dropdown">
-                                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                                <DropdownSubmenu href="#action/3.7" title="Text to show">
-                                                    <NavDropdown.Item href="#action/8.1">Sub 1</NavDropdown.Item>
-                                                    <DropdownSubmenu href="#action/3.7" title="Text to show">
-                                                        <NavDropdown.Item href="#action/9.1">
-                                                            Sub 2
-                                                        </NavDropdown.Item>
-                                                    </DropdownSubmenu>
-                                                </DropdownSubmenu>
-                                            </NavDropdownMenu>
-                                        </Nav>
-                                    </Navbar.Collapse>
-                                </Navbar>
+                                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                                <Nav className="mr-auto">
+                                    <NavDropdownMenu title="Products" id="collasible-nav-dropdown">
+                                        <DropdownSubmenu href="#action/3.7" title="Text to show">
+                                            <NavDropdown.Item href="#action/8.1">Sub 1</NavDropdown.Item>
+                                            <DropdownSubmenu href="#action/3.7" title="Text to show">
+                                                <NavDropdown.Item href="#action/9.1">  Sub 2</NavDropdown.Item>
+                                            </DropdownSubmenu>
+                                        </DropdownSubmenu>
+                                    </NavDropdownMenu>
+                                </Nav>
 
 
                                 <li className="nav-item">
