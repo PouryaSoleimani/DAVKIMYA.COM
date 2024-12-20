b45221ae3baec4d536c4525aef949f616a4ef643
PouryaSoleimani <pouryasoleimani93@gmail.com>
2024-12-20 17:52:14 +0330

    FOOTER

--- src/components/modules/DownloadButton.tsx
+++ src/components/modules/DownloadButton.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 
 const DownloadButton = () => {
    return (
+      
       <Link download={true} href="https://pouryasoleimani-adddesign.storage.c2.liara.space/DAV___KIMYA/DAV%20catalog.pdf" className="group relative inline-flex items-center justify-center w-[50px] h-[50px] bg-gradient-to-r from-blue-500 to-green-500 text-white rounded-full shadow-lg transform scale-100 transition-transform focus:outline-none focus:ring-4 focus:ring-blue-300 hover:scale-105 duration-500" target="_blank" rel="noopener noreferrer">
          <FaDownload className='w-6 h-6' />
       </Link>
